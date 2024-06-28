1e389c115fbb7211fdcd0768302c29c9795a1d6f
Simon Goller <neosam+github@posteo.de>
2024-06-28 13:43:58 +0200

    Dev

--- shifty-frontend.nix
+++ shifty-frontend.nix
@@ -7,8 +7,8 @@ let
     sha256 = "sha256-4Zu0RYRcAY/VWuu6awwq4opuiD//ahpc2aFHg2CWqFY=";
   }) {};
   src = pkgs.fetchzip {
-    url = "https://github.com/neosam/shifty-dioxus/releases/download/v0.1.5-dev03/shifty-frontend-v0.1.5-dev03.tgz";
-    sha256 = "sha256-PW9e2WraKEUNjACA9lfrhgIGKnpO7N1mOScv/JeTHv8=";
+    url = "https://github.com/neosam/shifty-dioxus/releases/download/v0.1.5-dev-04/shifty-frontend-v0.1.5-dev-04.tgz";
+    sha256 = "sha256-RWDJqXRda1aHMPttBD5Tz2dpIJEozmXpZvNFT9uWEqc=";
   };
   mkDerivation = specificPkgs.stdenv.mkDerivation;
 in
