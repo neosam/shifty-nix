bd7260b865d9bf72af0511cb370ffb774a2dc1ec
Simon Goller <neosam+github@posteo.de>
2024-12-14 00:07:06 +0100

    Prevent default button fix

--- shifty-frontend.nix
+++ shifty-frontend.nix
@@ -7,14 +7,14 @@ let
     sha256 = "sha256-IAoYyYnED7P8zrBFMnmp7ydaJfwTnwcnqxUElC1I26Y=";
   }) {};
   src = pkgs.fetchzip {
-    url = "https://github.com/neosam/shifty-dioxus/releases/download/v0.7.0-dev-05/shifty-frontend-v0.7.0-dev-05.tgz";
-    sha256 = "sha256-juU5qLzFGzn+OAj961Pa5Z2u/6R8KljidkZFNBEsW7c=";
+    url = "https://github.com/neosam/shifty-dioxus/releases/download/v0.7.0-dev-06/shifty-frontend-v0.7.0-dev-06.tgz";
+    sha256 = "sha256-jTwdvJv9r0sDLzDGO02ySur97Df5vSFfm0Zh8B01mPs=";
   };
   mkDerivation = specificPkgs.stdenv.mkDerivation;
 in
   mkDerivation {
     pname = "shifty-frontend";
-    version = "v0.7.0-dev-05";
+    version = "v0.7.0-dev-06";
     src = src;
 
     installPhase = ''
