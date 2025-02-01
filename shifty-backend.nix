e5a9d23e26fdf4a5a3b1f7010669711a51abc80e
Simon Goller <neosam+github@posteo.de>
2025-02-01 18:48:28 +0100

    Shifty update

--- shifty-backend.nix
+++ shifty-backend.nix
@@ -7,14 +7,14 @@ let
   src = specificPkgs.fetchFromGitHub {
     owner = "neosam";
     repo = "shifty-backend";
-    rev = "2c039f1";
-    sha256 = "sha256-Ln2F6DFjZXrhgpkLRdaD7rOCJeiX5kvuYa7avsLK+Fc=";
+    rev = "607558b";
+    sha256 = "sha256-YPLnb2Bl8YCV8ag/T7YF7niJt4Z11N4j1DDNr/maUuU=";
   };
   rustPlatform = specificPkgs.rustPlatform;
 in
   rustPlatform.buildRustPackage {
     pname = "shifty-service";
-    version = "2c039f1";
+    version = "607558b";
     src = src;
     buildFeatures = features;
     buildNoDefaultFeatures = true;
@@ -56,6 +56,6 @@ in
   chmod a+x $out/bin/start.sh
   '';
 
-    cargoHash = "sha256-izl0ukVEXnl08wyalr12s5luhjYm9L7flyz4iw+MoYo=";
+    cargoHash = "sha256-fIUcyeS6HaA3U/0Y/yTSRVF9po05m/f9GhzWCgd4DbU=";
   }
 
