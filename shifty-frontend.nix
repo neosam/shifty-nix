5ff565a572d6af91434fc5cc90f27757fa219f3a
Simon Goller <neosam+github@posteo.de>
2025-08-18 12:07:34 +0200

    Shfity release 1.2.1

--- shifty-frontend.nix
+++ shifty-frontend.nix
@@ -7,14 +7,14 @@ let
     sha256 = "sha256-IAoYyYnED7P8zrBFMnmp7ydaJfwTnwcnqxUElC1I26Y=";
   }) {};
   src = pkgs.fetchzip {
-    url = "https://github.com/neosam/shifty-dioxus/releases/download/v1.2.0/shifty-frontend-v1.2.0.tgz";
-    sha256 = "sha256-usEUcMFPJfhFQQc7EBptYFTlr+k/ju3rQsSa3WLK6Fo=";
+    url = "https://github.com/neosam/shifty-dioxus/releases/download/v1.2.1/shifty-frontend-v1.2.1.tgz";
+    sha256 = "sha256-c/YuIwh9ZrisMIcLrxWb0oM23Ugzi8tzb7rtlpFs0OU=";
   };
   mkDerivation = specificPkgs.stdenv.mkDerivation;
 in
   mkDerivation {
     pname = "shifty-frontend";
-    version = "v1.2.0";
+    version = "v1.2.1";
     src = src;
 
     installPhase = ''
