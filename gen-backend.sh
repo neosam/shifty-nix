a49209b073eb63a88d8af9b5a7bfca1f62166d26
Simon Goller <neosam+github@posteo.de>
2026-03-30 17:31:51 +0200

    update to version 1.10.0

--- gen-backend.sh
+++ gen-backend.sh
@@ -10,38 +10,38 @@ VERSION=$1
 TEMPLATE_PATH="./shifty-backend-template.nix"
 OUTPUT_PATH="./shifty-backend.nix"
 
-echo Finding hash for the git repository
-
+#echo Finding hash for the git repository
+#
 sed -e "s/__VERSION__/$VERSION/g" -e "s/__REPO_HASH__//g" -e "s/__CARGO_HASH__//g" $TEMPLATE_PATH > $OUTPUT_PATH
-
-output=$(nix-build $OUTPUT_PATH 2>&1)
-
-sha_line=$(echo "$output" | grep "got:\s*sha256-")
-
-echo $sha_line
-
-repoHash=$(echo "$sha_line" | sed 's/\s*got:\s*//')
-
-echo "'$repoHash'"
-
-sed -e "s/__VERSION__/$VERSION/g" -e "s|__REPO_HASH__|$repoHash|g" -e "s/__CARGO_HASH__//g" $TEMPLATE_PATH > $OUTPUT_PATH
-
-echo Finding cargo hash
-output=$(./build-oidc-backend.sh 2>&1)
-
-sha_line=$(echo "$output" | grep "got:\s*sha256-")
-
-echo $sha_line
-
-cargoHash=$(echo "$sha_line" | sed 's/\s*got:\s*//')
-
-echo "'$cargoHash'"
-
-sed -e "s/__VERSION__/$VERSION/g" -e "s|__REPO_HASH__|$repoHash|g" -e "s|__CARGO_HASH__|$cargoHash|g" $TEMPLATE_PATH > $OUTPUT_PATH
-
-set -e
-
-echo Finally, we can build
+#
+#output=$(nix-build $OUTPUT_PATH 2>&1)
+#
+#sha_line=$(echo "$output" | grep "got:\s*sha256-")
+#
+#echo $sha_line
+#
+#repoHash=$(echo "$sha_line" | sed 's/\s*got:\s*//')
+#
+#echo "'$repoHash'"
+#
+#sed -e "s/__VERSION__/$VERSION/g" -e "s|__REPO_HASH__|$repoHash|g" -e "s/__CARGO_HASH__//g" $TEMPLATE_PATH > $OUTPUT_PATH
+#
+#echo Finding cargo hash
+#output=$(./build-oidc-backend.sh 2>&1)
+#
+#sha_line=$(echo "$output" | grep "got:\s*sha256-")
+#
+#echo $sha_line
+#
+#cargoHash=$(echo "$sha_line" | sed 's/\s*got:\s*//')
+#
+#echo "'$cargoHash'"
+#
+#sed -e "s/__VERSION__/$VERSION/g" -e "s|__REPO_HASH__|$repoHash|g" -e "s|__CARGO_HASH__|$cargoHash|g" $TEMPLATE_PATH > $OUTPUT_PATH
+#
+#set -e
+#
+#echo Finally, we can build
 ./build-oidc-backend.sh
 
 echo $VERSION > backend-version.txt
--- gen-frontend.sh
+++ gen-frontend.sh
@@ -10,38 +10,38 @@ VERSION=$1
 TEMPLATE_PATH="./shifty-frontend-template.nix"
 OUTPUT_PATH="./shifty-frontend.nix"
 
-echo Finding hash for the git repository
+#echo Finding hash for the git repository
 
-sed -e "s/__VERSION__/$VERSION/g" -e "s/__REPO_HASH__//g" -e "s/__CARGO_HASH__//g" $TEMPLATE_PATH > $OUTPUT_PATH
-
-output=$(nix-build $OUTPUT_PATH 2>&1)
-
-sha_line=$(echo "$output" | grep "got:\s*sha256-")
-
-echo $sha_line
-
-repoHash=$(echo "$sha_line" | sed 's/\s*got:\s*//')
-
-echo "'$repoHash'"
-
-sed -e "s/__VERSION__/$VERSION/g" -e "s|__REPO_HASH__|$repoHash|g" -e "s/__CARGO_HASH__//g" $TEMPLATE_PATH > $OUTPUT_PATH
-
-echo Finding cargo hash
-output=$(./build-frontend.sh 2>&1)
-
-sha_line=$(echo "$output" | grep "got:\s*sha256-")
-
-echo $sha_line
-
-cargoHash=$(echo "$sha_line" | sed 's/\s*got:\s*//')
-
-echo "'$cargoHash'"
-
-sed -e "s/__VERSION__/$VERSION/g" -e "s|__REPO_HASH__|$repoHash|g" -e "s|__CARGO_HASH__|$cargoHash|g" $TEMPLATE_PATH > $OUTPUT_PATH
-
-set -e
-
-echo Finally, we can build
+#sed -e "s/__VERSION__/$VERSION/g" -e "s/__REPO_HASH__//g" -e "s/__CARGO_HASH__//g" $TEMPLATE_PATH > $OUTPUT_PATH
+#
+#output=$(nix-build $OUTPUT_PATH 2>&1)
+#
+#sha_line=$(echo "$output" | grep "got:\s*sha256-")
+#
+#echo $sha_line
+#
+#repoHash=$(echo "$sha_line" | sed 's/\s*got:\s*//')
+#
+#echo "'$repoHash'"
+#
+#sed -e "s/__VERSION__/$VERSION/g" -e "s|__REPO_HASH__|$repoHash|g" -e "s/__CARGO_HASH__//g" $TEMPLATE_PATH > $OUTPUT_PATH
+#
+#echo Finding cargo hash
+#output=$(./build-frontend.sh 2>&1)
+#
+#sha_line=$(echo "$output" | grep "got:\s*sha256-")
+#
+#echo $sha_line
+#
+#cargoHash=$(echo "$sha_line" | sed 's/\s*got:\s*//')
+#
+#echo "'$cargoHash'"
+#
+#sed -e "s/__VERSION__/$VERSION/g" -e "s|__REPO_HASH__|$repoHash|g" -e "s|__CARGO_HASH__|$cargoHash|g" $TEMPLATE_PATH > $OUTPUT_PATH
+#
+#set -e
+#
+#echo Finally, we can build
 ./build-frontend.sh
 
 echo $VERSION > frontend-version.txt
