ec1f9a36559548875a11976b2536af414cdb760b
Simon Goller <neosam+github@posteo.de>
2026-07-05 10:14:34 +0200

    2.4.0

--- shifty-backend.nix
+++ shifty-backend.nix
@@ -1,11 +1,11 @@
 { features ? [], pkgs ? import <nixpkgs> {} }:
 let
-  backend = (builtins.getFlake "https://github.com/neosam/shifty-backend/archive/9cbe151d6e8a.zip").packages.${pkgs.system}.backend-oidc;
+  backend = (builtins.getFlake "https://github.com/neosam/shifty-backend/archive/v2.4.0.zip").packages.${pkgs.system}.backend-oidc;
 in
   backend
 #  rustPlatform.buildRustPackage {
 #    pname = "shifty-service";
-#    version = "9cbe151d6e8a";
+#    version = "v2.4.0";
 #    src = src;
 #    nativeBuildInputs = with specificPkgs; [curl];
 #    buildFeatures = features;
