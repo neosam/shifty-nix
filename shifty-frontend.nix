d184d96f2c4dd6dbc01d2e6f3befa5c3792770bb
Simon Goller <neosam+github@posteo.de>
2026-07-06 16:09:00 +0200

    2.5.0

--- shifty-frontend.nix
+++ shifty-frontend.nix
@@ -1,5 +1,5 @@
 { features ? [], pkgs ? import <nixpkgs> {} }:
 let
-  frontend = (builtins.getFlake "https://github.com/neosam/shifty-backend/archive/5222d53b6156.zip").packages.${pkgs.system}.frontend;
+  frontend = (builtins.getFlake "https://github.com/neosam/shifty-backend/archive/v2.5.0.zip").packages.${pkgs.system}.frontend;
 in
   frontend
